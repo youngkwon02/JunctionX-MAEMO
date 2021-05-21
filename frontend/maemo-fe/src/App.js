dd3d612afd7c7b7a2e448115739e4797ffcb3032
jekoo <pjkwprn@gmail.com>
2021-05-21 22:24:38 +0900

    feat : routing

--- frontend/maemo-fe/src/App.js
+++ frontend/maemo-fe/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MainView from './views/MainView'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
-            {/*  */}
+          <Route exact path="/" component={MainView}></Route>
         </Switch>
       </BrowserRouter>
     </>
