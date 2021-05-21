fc3964f9be89aabc9bd6188d6bdde10bea5716e3
youngkwon02 <youngkwon02@naver.com>
2021-05-21 22:47:33 +0900

    feat : create submitBtn.jsx

---
+++ frontend/maemo-fe/src/views/LoginView.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SubmitButton from "../components/LoginView/button/submitButton";
+
+
+const LoginView = () => {
+  return (
+    <>
+      <SubmitButton>
+          Submit
+      </SubmitButton>
+    </>
+  );
+}
+
+export default LoginView;
