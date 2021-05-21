ab6e9d927e496a3e56a3bb4d0f007c4f613f8c44
jekoo <pjkwprn@gmail.com>
2021-05-22 04:39:12 +0900

    feat : timeView format

---
+++ frontend/maemo-fe/src/components/TimeView/input/input.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NumInput = styled.input`
+  width: 50px;
+`
+
+const Input = () => {
+  return (
+    <>
+      <NumInput></NumInput>
+    </>
+  )
+}
+
+export default Input
--- frontend/maemo-fe/src/views/TimeView.jsx
+++ frontend/maemo-fe/src/views/TimeView.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
+import SubmitButton from '../components/TimeView/button/submitButton'
+import Input from '../components/TimeView/input/input'
+
+const MainInfo = styled.div`
+  text-align: center;
+	font-weight: bold;
+	margin-bottom: 30px;
+`
+
+const Tags = styled.span`
+  font-weight: bold;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 10%;
+`
 
 const TimeView = () => {
   return (
     <>
-      <h1>timeView</h1>
+      <MainInfo>언제 이동하실 예정인가요?</MainInfo>
+      <Wrapper>
+        <Tags>날짜</Tags>
+        <Input></Input>
+        <Tags>년</Tags>
+        <Input></Input>
+        <Tags>월</Tags>
+        <Input></Input>
+        <Tags>일</Tags>
+      </Wrapper>
+      <Wrapper>
+        <Tags>시간</Tags>
+        <Input></Input>
+        <Tags>시</Tags>
+        <Input></Input>
+        <Tags>분</Tags>
+      </Wrapper>
+      <SubmitButton>다음</SubmitButton>
     </>
   )
 }
