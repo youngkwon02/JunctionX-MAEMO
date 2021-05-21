1988eaefa7165b26db29bc8475e7e2a4e77900da
youngkwon02 <youngkwon02@naver.com>
2021-05-22 01:41:23 +0900

    fix : login button style

--- frontend/maemo-fe/src/components/LoginView/button/submitButton.jsx
+++ frontend/maemo-fe/src/components/LoginView/button/submitButton.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledSubmitButton = styled.button`
-    width: 80%;
+    width: 100%;
     height: 50px;
     background-color: #fef000;
     color: black;
     border: none;
     font-weight: bold;
     margin-top: 30px;
+    border-radius: 14px;
 `;
 
 const SubmitButton = ({children}) => {
