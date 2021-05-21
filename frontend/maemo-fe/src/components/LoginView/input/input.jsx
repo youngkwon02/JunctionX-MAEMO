9f12b0b0cd760d514a47fe4298bfb4f54202f613
youngkwon02 <youngkwon02@naver.com>
2021-05-22 01:29:07 +0900

    refactor : login layout

--- frontend/maemo-fe/src/components/LoginView/input/input.jsx
+++ frontend/maemo-fe/src/components/LoginView/input/input.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import styled, {css} from 'styled-components';
 
 const StyledInput = styled.input`
-    width: 100%;
+    width: calc(100% - 20px);
     height: 30px;
     color: black;
     border: none;
     border-bottom: 2px solid rgba(10, 10, 10, .1);
     font-weight: bold;
     margin-bottom: 10px;
+    padding: 0 10px;
     ${(props) =>
         props.relatePhone &&
         css`
           display: inline-block;
-          width: 74%;
+          width: 72%;
           padding-bottom: 0;
     `}
 `;
