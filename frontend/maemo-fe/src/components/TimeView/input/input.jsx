908b4f273eca7ea90dcff26d3bb022ebcf8ce7d9
youngkwon02 <youngkwon02@naver.com>
2021-05-22 06:45:39 +0900

    refactor : TimeView style

--- frontend/maemo-fe/src/components/TimeView/input/input.jsx
+++ frontend/maemo-fe/src/components/TimeView/input/input.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 const NumInput = styled.input`
-  width: 50px;
+  width: 40px;
+  height: 30px;
+  border-radius: 8px;
+  box-shadow: 1px 1px 1px rgba(10, 10, 10, .2);
+  border: 1px solid rgba(10, 10, 10, .2);
 `
 
 const Input = ({onChange}) => {
