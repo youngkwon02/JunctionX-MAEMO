41dca88e34b100b1a1fcb6ae69eaf89eaf184871
jekoo <pjkwprn@gmail.com>
2021-05-23 00:19:49 +0900

    feat : modify enterView SVG file

--- frontend/maemo-fe/src/views/EnterView.jsx
+++ frontend/maemo-fe/src/views/EnterView.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import styled from 'styled-components'
 import { useHistory } from 'react-router-dom';
-import enterImg from '../assets/enter.svg'
+import { ReactComponent as MainSVG } from '../assets/main.svg'
 
 const EnterView = () => {
   const history = useHistory();
 
   return (
-    <div style={{width: "100%", height: "100vh"}}>
-    <img width="100%" height="100%" src={enterImg}></img>
-    </div>
+    <>
+    <MainSVG onClick={() => history.push("/main")} width="100%" height="100%"></MainSVG>
+    </>
   );
-  
 }
 
 export default EnterView;
