7c63d160ca86ea0f68bed7aa5229554e263ea5bc
ohjeeyoung <jeeyoung990725@naver.com>
2021-05-22 17:10:40 +0900

    refactor: delete aws app key

--- backend/maemo_rest_api/location/notification.py
+++ backend/maemo_rest_api/location/notification.py
@@ -10,8 +10,8 @@ class NotificationClient:
     def __init__(self):
         self.client = boto3.client(
             NotificationClient.NotificationType.SNS,
-            aws_access_key_id="AKIAYSFA6KP4USZMI77C",
-            aws_secret_access_key="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",
+            aws_access_key_id="",
+            aws_secret_access_key="",
             region_name=NotificationClient.NotificationType.REGION_NORTHEAST_2
         )
 
@@ -20,20 +20,5 @@ class NotificationClient:
             PhoneNumber=phone_number,
             Message=message
         )
-
-
-
-# topic_arn = 'arn:aws:sns:ap-northeast-1:xxxxxxxx:My-Alert'
-# client.subscribe(
-# TopicArn=topic_arn,
-# Protocol='sms',
-# Endpoint='+8201012345678'
-# )
-
-# client.publish(
-# TopicArn=topic_arn ,
-# Message="파이썬 코드로 문자 보내기"
-# )
-
 
 
