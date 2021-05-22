c2ed10439b21054bbc4514b1078b541bc43d6263
ohjeeyoung <jeeyoung990725@naver.com>
2021-05-23 04:50:44 +0900

    refactor: fix post_sns api

--- backend/maemo_rest_api/location/notification.py
+++ backend/maemo_rest_api/location/notification.py
@@ -1,24 +1,19 @@
 import boto3
 
 
-class NotificationClient:
-    class NotificationType:
-        SNS = "sns"
-        PUSH_NOTIFICATION = "push_notification"
-        REGION_NORTHEAST_2 = "ap-northeast-1"
+class NotificationType:
+    SNS = "sns"
+    PUSH_NOTIFICATION = "push_notification"
+    REGION_NORTHEAST_2 = "ap-northeast-1"
 
-    def __init__(self):
-        self.client = boto3.client(
-            NotificationClient.NotificationType.SNS,
-            aws_access_key_id="",
-            aws_secret_access_key="",
-            region_name=NotificationClient.NotificationType.REGION_NORTHEAST_2
-        )
 
-    def publish_message(self, phone_number, message):
-        self.client.publish(
-            PhoneNumber=phone_number,
-            Message=message
-        )
+client = boto3.client(
+    NotificationType.SNS,
+        aws_access_key_id="AKIAYSFA6KP44AFYGKJK",
+        aws_secret_access_key="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",
+        region_name=NotificationType.REGION_NORTHEAST_2
+    )
+
+        
 
 
--- backend/maemo_rest_api/location/views.py
+++ backend/maemo_rest_api/location/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework import permissions, status, generics
 from rest_framework.response import Response
-from location.notification import NotificationClient
+from location.notification import client
 import jwt
 from maemo_rest_api import settings
 from accounts.models import User
@@ -31,12 +31,12 @@ class NotificationAPI(APIView):
         target_user = User.objects.get(id=auth['user_id'])
         parsed_phone_number = target_user.protector_phone
         parsing_phone_number = '+82'+str(''.join(parsed_phone_number.split('-')))
-        print(parsing_phone_number)
+        phone = parsing_phone_number[0:3] + parsing_phone_number[4:]
+        print(phone)
 
-        notification_client = NotificationClient()
-        notification_client.publish_message(
-            phone_number= parsing_phone_number, 
-            message='예상 경로의 반경을 2km 이상 이탈했습니다. 보호자께서는 오지영님의 상황을 확인해주세요! 장시간 동안 연락이 되지 않는다면 긴급신고 112로 전화 부탁드립니다.'
+        client.publish(
+            PhoneNumber= phone, 
+            Message='예상 경로의 반경을 2km 이상 이탈했습니다. 보호자께서는 오지영님의 상황을 확인해주세요! 장시간 동안 연락이 되지 않는다면 긴급신고 112로 전화 부탁드립니다.'
         )
         return Response(status.HTTP_200_OK)
 
