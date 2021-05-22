98715a61c493170f5911de01e225307509896b06
youngkwon02 <youngkwon02@naver.com>
2021-05-23 01:39:07 +0900

    feat : create warning layer

--- frontend/maemo-fe/src/views/TrackingView.jsx
+++ frontend/maemo-fe/src/views/TrackingView.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components'
 import TrackingPath from '../assets/tracking.json'
+import Layer from '../components/TrackingView/layer/layer'
 import marker from '../assets/marker.svg'
 
 const TrackingView = () => {
@@ -14,14 +15,38 @@ const TrackingView = () => {
     
     const script = document.createElement("script");
     script.innerHTML = `
+
+
+        const getPoint = (pointX, pointY) => {
+          return {"x": pointX, "y": pointY};
+        }
+        
+        const getDistance = (pointA, pointB) => {
+          diffX = (pointA.x - pointB.x <= 0) ? pointB.x - pointA.x : pointA.x - pointB.x;
+          diffY = (pointA.y - pointB.y <= 0) ? pointB.y - pointA.y : pointA.y - pointB.y;
+          return Math.sqrt((diffX**2) + (diffY ** 2));
+        }
+
+        const isDeviation = (centerPoint, targetPoint, radius) => {
+          let distance = getDistance(centerPoint, targetPoint);
+          if(distance > radius) {
+            return true;
+          }else {
+            return false;
+          }
+        }
+        
         var map;
         var markers = [];
         const startX = parseFloat(document.querySelector('#startX').value);
         const startY = parseFloat(document.querySelector('#startY').value);
+        const startPoint = getPoint(startX, startY);
         const endX = parseFloat(document.querySelector('#endX').value);
         const endY = parseFloat(document.querySelector('#endY').value);
         const centerX = (startX + endX) / 2;
         const centerY = (startY + endY) / 2;
+        const centerPoint = getPoint(centerX, centerY);
+        const radius = getDistance(startPoint, centerPoint);
         const JsonObj = JSON.parse(document.querySelector('#jsonData').value);
         function initTmap() {
           map = new Tmapv2.Map("TMapApp", {
@@ -37,14 +62,22 @@ const TrackingView = () => {
               position: new Tmapv2.LatLng(JsonObj[index].x,JsonObj[index].y), //Marker의 중심좌표 설정.
               map: map //Marker가 표시될 Map 설정..
             });
-          setInterval(()=>{
-            let markersEle = document.querySelectorAll("#TMapApp > div > div:nth-child(3) > img");
-            for(let i=0; i<markersEle.length; i++) {
-              if(markersEle[i].src != document.querySelector('#marker').value){
-                markersEle[i].src = document.querySelector('#marker').value;
-              }
+
+            if(isDeviation(centerPoint, JsonObj[index], radius)) {
+              console.log("Deviation");
+              document.querySelector('#warning-layer').style.display = "block";
+            }else {
+              console.log("Not Deviation");
             }
-          }, 100);
+
+            setInterval(()=>{
+              let markersEle = document.querySelectorAll("#TMapApp > div > div:nth-child(3) > img");
+              for(let i=0; i<markersEle.length; i++) {
+                if(markersEle[i].src != document.querySelector('#marker').value){
+                  markersEle[i].src = document.querySelector('#marker').value;
+                }
+              }
+            }, 100);
             index += 1;
             if(index >= JsonObj.length) {
               clearInterval(interval);
@@ -52,7 +85,6 @@ const TrackingView = () => {
           }, 1000);
         }
         initTmap();
-        
    `;
     script.type = "text/javascript";
     script.async = "async";
@@ -78,13 +110,15 @@ const TrackingView = () => {
         position: "fixed",
        }}
     />
+    <Layer id="warning-layer">예상 경로를<br/>벗어났습니다.<br/><br/>보호자에게 문자를 전송합니다.</Layer>
     <BottomDiv>
-    <TrackingInput type="text" id="jsonData"></TrackingInput>
+    <TrackingInput id="jsonData" type="text"></TrackingInput>
     <TrackingInput id="startX" type="text"></TrackingInput>
     <TrackingInput id="startY" type="text"></TrackingInput>
     <TrackingInput id="endX" type="text"></TrackingInput>
     <TrackingInput id="endY" type="text"></TrackingInput>
     <TrackingInput id="marker" type="text"></TrackingInput>
+    <TrackingInput id="deviationState" type="text"></TrackingInput>
     </BottomDiv>
     </>
   );
