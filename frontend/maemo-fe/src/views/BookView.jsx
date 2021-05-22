9ecc71f9f296d31f3ad394c38f9ac358e90fc852
jekoo <pjkwprn@gmail.com>
2021-05-22 18:49:33 +0900

    refactor : booklist api

--- frontend/maemo-fe/src/views/BookView.jsx
+++ frontend/maemo-fe/src/views/BookView.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import Container from '../components/BookView/div/container'
 import BookElem from "../components/BookView/div/bookElem";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import { getRandomKey } from '../utils/random'
 import { useHistory } from 'react-router-dom';
+import { getAxios } from '../api/axios';
+import axios from 'axios';
 
 const bookList = [
   {
@@ -20,26 +22,40 @@ const bookList = [
     "end": "중앙대학교 정문"
   }
 ];
+const BookTitle = styled.div`
+font-size: 1.2em;
+text-align: center;
+font-weight: bold;
+margin-bottom: 30px;
+`;
+
+const BackIconBox = styled.div`
+width: 20px;
+height: 20px;
+`
 
 const BookView = () => {
   const history = useHistory();
- 
-  const BookTitle = styled.div`
-    font-size: 1.2em;
-		text-align: center;
-		font-weight: bold;
-		margin-bottom: 30px;
-	`;
+  const [data, setData] = useState({})
+
+  const getBookList = async () => {
+    try {
+      const res = await getAxios('/reservation')
+      setData(res)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getBookList()
+  }, [])
 
   const Books = bookList.map((book) => (<BookElem key={getRandomKey()} data={book}></BookElem>));
 
-  const BackIconBox = styled.div`
-    width: 20px;
-    height: 20px;
-  `
-
   return (
     <Container>
+      {data}
       <BackIconBox>
         <ArrowBackIcon onClick={() => history.push("/main")}></ArrowBackIcon>
       </BackIconBox>
