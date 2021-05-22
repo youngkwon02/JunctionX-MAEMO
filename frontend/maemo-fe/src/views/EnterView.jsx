ee8b1ce9488adf24fa31622410b322551c27ebf2
youngkwon02 <youngkwon02@naver.com>
2021-05-22 13:39:52 +0900

    feat : create EnterView

--- frontend/maemo-fe/src/views/EnterView.jsx
+++ frontend/maemo-fe/src/views/EnterView.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from 'styled-components'
 import { useHistory } from 'react-router-dom';
+import enterImg from '../assets/enter.svg'
 
 const MainView = () => {
   const history = useHistory();
 
   return (
-    <>
-    </>
+    <div style={{width: "100%", height: "100vh"}}>
+    <img width="100%" height="100%" src={enterImg}></img>
+    </div>
   );
   
 }
