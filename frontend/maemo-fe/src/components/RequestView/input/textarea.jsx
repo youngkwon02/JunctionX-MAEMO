94b0309a2caf62112ded9b7473fe04b6a69d6dbc
youngkwon02 <youngkwon02@naver.com>
2021-05-22 18:37:26 +0900

    refactor : requestView

---
+++ frontend/maemo-fe/src/components/RequestView/input/textarea.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTextArea = styled.textarea`
+    color: black;
+    border: none;
+		word-break: keep-all;
+    width: calc(100% - 40px);
+    height: 160px;
+    border: 1px solid rgba(0, 0, 0, .2);
+    border-radius: 14px;
+    resize: none;
+    padding: 20px;
+`;
+
+const TextArea = ({children, ...props}) => {
+    return (
+        <>
+        	<StyledTextArea onChange={props.onChange}>
+          {children}
+          </StyledTextArea>
+        </>
+    )
+}
+
+export default TextArea;
--- frontend/maemo-fe/src/views/RequestView.jsx
+++ frontend/maemo-fe/src/views/RequestView.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Container from '../components/RequestView/div/container'
+import Hello from '../components/RequestView/div/hello'
+import TextArea from '../components/RequestView/input/textarea'
+import BookingButton from '../components/RequestView/button/booking'
 import {useLocation} from 'react-router-dom'
 
 const RequestView = () => {
   const location = useLocation()
+  const [msg, setMsg] = useState("")
   console.log(location.state)
+  const requestHandler = (e) => {
+    setMsg(e.target.value)
+  }
+
+  const clickHandler = () => {
+    console.log("h",msg)
+    console.log({msg, ...location.state})
+  }
+
   return (
-    <div>{123}</div>
+    <Container>
+      <Hello></Hello>
+      <TextArea onChange={requestHandler}></TextArea>
+      <BookingButton onClick={clickHandler}>예약하기</BookingButton>
+    </Container>
   )
 }
 
