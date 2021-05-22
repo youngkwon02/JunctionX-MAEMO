d49060b78148930d479514ac399ac357831f710d
ohjeeyoung <jeeyoung990725@naver.com>
2021-05-23 01:12:49 +0900

    feat: add get_difference actual_taxi_fare and expected_taxi_fare

--- backend/maemo_rest_api/maemo_rest_api/urls.py
+++ backend/maemo_rest_api/maemo_rest_api/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
 from maemo_rest_api.global_utils.jwt_token import validate_jwt_token
-from location.views import NotificationAPI
+from location.views import NotificationAPI, LocationAPI
 from reservation.views import TaxiAPI
 
 
@@ -15,6 +15,7 @@ urlpatterns = [
     path('refresh/', refresh_jwt_token),
     path('user', include('accounts.urls')),
     path('reservation', include('reservation.urls')),
-    path('test', NotificationAPI.as_view()),
+    path('notification', NotificationAPI.as_view()),
+    path('location', LocationAPI.as_view()),
     path('taxi', TaxiAPI.as_view())
 ]
